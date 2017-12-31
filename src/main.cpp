974fb2e6d8655d9487ad2b72735de05056c00320
Justin Lee <justin.lee007@gmail.com>
2017-12-30 22:20:19 -0600

    Added first sensor fusion mechanism for lane and speed of surrounding cars

--- src/main.cpp
+++ src/main.cpp
@@ -184,13 +184,14 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  // Start in lane 1
+  static int lane = 1;
+
+  // Have a reference velocity to target
+  static double ref_vel = 0; // mph
+
   h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                                                                                                            uWS::OpCode opCode) {
-    // Start in lane 1
-    int lane = 1;
-
-    // Have a reference velocity to target
-    double ref_vel = 49.5; // mph
 
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -250,8 +251,7 @@ int main() {
               // Check s values greater than mine and s gap
               if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
                 // Do some logic here, lower reference velocity so we don't crash into the car in front of us, could also flag to try to change lanes.
-                ref_vel = 29.5; // mph
-                //too_close = true
+                too_close = true;
               }
             }
           }
